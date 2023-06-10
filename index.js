06267612a97d091992812b976e6f0a9aa85334a5
“AnonnaGH” <anonnaferdosh193@gmail.com>
2023-06-10 14:13:41 +0600

    user post api added

--- index.js
+++ index.js
@@ -33,6 +33,27 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+
+        const usersCollection = client.db("graphic_db").collection("users");
+
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const query = { email: user.email }
+            const existingUser = await usersCollection.findOne(query);
+            console.log('existing user', existingUser)
+            if (existingUser) {
+                return res.send({ message: 'user already exists' })
+            }
+
+
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
