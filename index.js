11e91c8dafed97ab91bec785d570c656df2d1f28
“AnonnaGH” <anonnaferdosh193@gmail.com>
2023-06-10 13:14:40 +0600

    basic server setup

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+require('dotenv').config()
+const port = process.env.PORT || 5000
+
+
+
+
+
+
+// middleware
+app.use(cors());
+app.use(express.json())
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Graphic School server is running!')
+})
+
+
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`)
+})
+
+
