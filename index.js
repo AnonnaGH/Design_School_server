ee278a5f1afed845a0e08253dbbfcde523a95e9f
“AnonnaGH” <anonnaferdosh193@gmail.com>
2023-06-10 20:44:37 +0600

    admin and instructor check api

--- index.js
+++ index.js
@@ -16,7 +16,26 @@ const port = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json())
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+    console.log(authorization)
+    // bearer token
+    const token = authorization.split(' ')[1];
 
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            console.log(err)
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
+
+        }
+
+        req.decoded = decoded;
+        next();
+    })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2fbz8ar.mongodb.net/?retryWrites=true&w=majority`;
@@ -90,6 +109,20 @@ async function run() {
 
         })
 
+        // check admin
+        app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+
+            if (req.decoded.email !== email) {
+                res.send({ admin: false })
+            }
+
+            const query = { email: email }
+            const user = await usersCollection.findOne(query);
+            const result = { admin: user?.role === 'admin' }
+            res.send(result);
+        })
+
 
 
         // make instructor api
@@ -109,6 +142,22 @@ async function run() {
             res.send(result);
 
         })
+
+
+        // check instructor
+        app.get('/users/instructor/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+
+            if (req.decoded.email !== email) {
+                res.send({ instructor: false })
+            }
+
+            const query = { email: email }
+            const user = await usersCollection.findOne(query);
+            const result = { instructor: user?.role === 'instructor' }
+            res.send(result);
+        })
+
 
 
         // Send a ping to confirm a successful connection
